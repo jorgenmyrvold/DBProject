2e0394802cfe84aa21d978dd021729f2dda5c25e
DESKTOP-306INQD\Sigmund <sigmund.mestad@tekonologiporten.no>
2019-03-19 16:32:30 +0100

    Laga tekst-meny

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,38 @@ public class Main {
 	}
 	
 	
-	
-
-	public static void regTrening() {
+	public static void run() {
 		Scanner reader = new Scanner(System.in);
+		String menu = ("Hva vil du gjøre? (0-?)\n" +
+				"1) Registrer trening \n" +
+				"2) Se siste treningsøkter\n" +
+				"3) ???\n" +
+				"0) Avslutt\n");
+		String ans = "";
+		
+		while (ans != "0") {
+			System.out.println(menu);
+			ans = reader.next();
+			switch(ans) {
+			case "1":
+				regTrening(reader);
+				break;
+			case "2":
+				System.out.println("Henter ut liste over treningsøkter");
+				break;
+			case "3":
+				System.out.println("Ett eller anna");
+				break;
+			case "0":
+				reader.close();
+				return;
+			default:
+				System.out.println("Input må vere et tall mellom 0 og 3");
+			}
+		}
+	}
+
+	public static void regTrening(Scanner reader) {
 		String tidspunkt = Helpers.getDatetime(reader);
 		int varighet = Helpers.getVarighet(reader);
 		int form = Helpers.getForm(reader);
@@ -25,32 +53,11 @@ public class Main {
 		
 		
 		RegTreningCtrl regTrening = new RegTreningCtrl();
-//		regTrening.connect();
-//		regTrening.startReg();
 		regTrening.regTrening(tidspunkt, varighet, form, prestasjon, notat);
 	}
 
-// Registrer Ã¸velse med med overgangstabell
-//	public static void regOvelse() {
-//		Scanner reader = new Scanner(System.in);
-//		String ovelseNavn = Helpers.getOvelseNavn(reader);
-//		String ovelseBeskrivelse = Helpers.getOvelseBeskrivelse(reader);
-//		Boolean isApparat = Helpers.getIsApparat(reader);
-//
-//        RegOvelseCtrl regOvelse = new RegOvelseCtrl();
-//		if (isApparat){
-//		    String apparatNavn = Helpers.getApparatNavn(reader);
-//              regOvelse.regApparatOvelse(ovelseNavn, ovelseBeskrivelse, apparatNavn);
-//        }
-//        else {
-//            regOvelse.regFriOvelse(ovelseNavn, ovelseBeskrivelse);
-//        }
-//        reader.close();
-//	}
 
-// Registrer Ã¸velse uten ekstra tabell, funker ikke heilt
-    public static void regOvelse() {
-        Scanner reader = new Scanner(System.in);
+    public static void regOvelse(Scanner reader) {
         String ovelseNavn = Helpers.getOvelseNavn(reader);
         String ovelseBeskrivelse = Helpers.getOvelseBeskrivelse(reader);
         Boolean isApparat = Helpers.getIsApparat(reader);
@@ -79,8 +86,8 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Main main = new Main();
-
-		regOvelse();
+		run();
+//		regOvelse();
 //		regTrening();
 //        regApparat();
 		
